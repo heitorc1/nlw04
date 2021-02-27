689461ff1899817fb1cc16829c9bc3f43cfbf99c
Heitor Carneiro <heitorcarneiro1@gmail.com>
2021-02-26 22:35:00 -0300

    Add files via upload

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,38 @@
+import { getConnection } from 'typeorm';
+import request from 'supertest';
+import { app } from "../app";
+import createConnection from '../database';
+
+
+describe("User", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    afterAll(async () => {
+        const connecetion = getConnection();
+        await connecetion.dropDatabase();
+        await connecetion.close();
+
+    })
+
+    it("Should be able to create a new user", async () => {
+        const response = await request(app).post("/users")
+            .send({
+                email: "user@example.com",
+                name: "User Example"
+            });
+        expect(response.status).toBe(201);
+    });
+
+    it("Should not be able to create a user with exists email", async () => {
+        const response = await request(app).post("/users")
+            .send({
+                email: "user@example.com",
+                name: "User Example"
+            });
+        expect(response.status).toBe(400);
+    });
+
+});
